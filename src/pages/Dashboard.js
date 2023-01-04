3ca63d31ad36f22cb21dc5c0a0f1d4cccf0f50b9
Your Name <bajiraorandive9@gmail.com>
2023-01-04 21:55:31 +0530

    committed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,37 @@
 import React from "react";
-import Sidebar from "../components/SideBar";
-import {Routes, Route} from "react-router-dom";
+import Col from 'react-bootstrap/esm/Col';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
 import DashPage from "../components/DashPage";
 
 const Dashboard = () => {
     return (
         <>
-        <DashPage/>
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+        <Row>
+          <Col>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">Tab 2</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={10}>
+            <Tab.Content className="showtab">
+              <Tab.Pane eventKey="first">
+                <DashPage/>
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">
+                =
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
         </>
     );
 };
